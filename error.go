e49d5bb9b41104777442ad910f2918c68271b7fa
Dali Kewara <dalikewara@gmail.com>
2022-09-19 17:15:57 +0700

    update error.go

--- error.go
+++ error.go
@@ -6,52 +6,71 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
+const DBCodeColumnNotExists = "42703"
+const DBCodeDuplicateKey = "23505"
+const DBCodeInvalidInputSyntax = "22P02"
+
 type ErrDatabase struct {
-	err     error
-	code    string
-	message string
+	DBErr     error
+	DBCode    string
+	DBMessage string
 }
 
 func ErrDB(e error) *ErrDatabase {
 	err := &ErrDatabase{
-		err: e,
+		DBErr: e,
 	}
 	err.extract()
 	return err
 }
 
+func NewMockErrDB(code, message string) error {
+	err := &ErrDatabase{
+		DBErr:     errors.New(code + "||" + message),
+		DBCode:    code,
+		DBMessage: message,
+	}
+	return err
+}
+
 func (e *ErrDatabase) Error() string {
-	return e.err.Error()
+	return e.DBErr.Error()
 }
 
 func (e *ErrDatabase) Code() string {
-	return e.code
+	return e.DBCode
 }
 
 func (e *ErrDatabase) Message() string {
-	return e.message
+	return e.DBMessage
 }
 
-func (e *ErrDatabase) NoRows() bool {
-	return e.err.Error() == pgx.ErrNoRows.Error()
+func (e *ErrDatabase) IsNoRows() bool {
+	return e.DBErr.Error() == pgx.ErrNoRows.Error()
 }
 
-func (e *ErrDatabase) ColumnNotExists() bool {
-	return e.code == "42703"
+func (e *ErrDatabase) IsColumnNotExists() bool {
+	return e.DBCode == DBCodeColumnNotExists
 }
 
-func (e *ErrDatabase) DuplicateKey() bool {
-	return e.code == "23505"
+func (e *ErrDatabase) IsDuplicateKey() bool {
+	return e.DBCode == DBCodeDuplicateKey
 }
 
-func (e *ErrDatabase) InvalidInputSyntax() bool {
-	return e.code == "22P02"
+func (e *ErrDatabase) IsInvalidInputSyntax() bool {
+	return e.DBCode == DBCodeInvalidInputSyntax
 }
 
 func (e *ErrDatabase) extract() {
 	var pgErr *pgconn.PgError
-	if errors.As(e.err, &pgErr) {
-		e.code = pgErr.Code
-		e.message = pgErr.Message
+	if errors.As(e.DBErr, &pgErr) {
+		e.DBCode = pgErr.Code
+		e.DBMessage = pgErr.Message
+	} else {
+		var errDB *ErrDatabase
+		if errors.As(e.DBErr, &errDB) {
+			e.DBCode = errDB.DBCode
+			e.DBMessage = errDB.DBMessage
+		}
 	}
 }
